33596180e7e0faf1eca7075e6f77c9d8b4d45794
darshanitaliya7110 <darshan.italiya@pixer.digital>
2024-07-30 15:42:55 +0530

    display blog

--- src/component/BlogList.jsx
+++ src/component/BlogList.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+"use client"
+
+import axios from 'axios';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import React, { useCallback, useEffect, useState } from 'react'
 
 const BlogList = () => {
+
+    const [blogData, setBlogData] = useState(null)
+
+    const query = `{
+                        blogCollection {
+                            items {
+                            _id
+                            title
+                            image{
+                                title
+                                url
+                            }
+                            body{
+                                json
+                            }
+                            author{
+                                authorName
+                                authorImage{
+                                url
+                                }
+                            }
+                            date
+                            }
+                        }
+                    }`;
+
+    const getData = useCallback(async () => {
+        const tempData = await axios.post(
+            `https://graphql.contentful.com/content/v1/spaces/${process.env.NEXT_PUBLIC_SPACE_ID}`,
+            { query },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`
+                }
+            }
+        )
+        setBlogData(tempData.data.data.blogCollection.items)
+    }, [query])
+
+    useEffect(() => {
+        getData()
+    }, [getData])
+
+    console.log(blogData)
+
+
     return (
         <div>
-            BlogList
+            {blogData?.map((item) =>
+                <div key={item._id}>
+                    <h1>{item.title}</h1>
+                    <img src={item.image.url} alt={item.image.title} />
+                    <div>{documentToReactComponents(item.body.json)}</div>
+                    <span><p><img style={{ width: "30px", height: "30px" }} src={item.author.authorImage.url} alt={item.author.authorName} /> {item.author.authorName}</p></span>
+                    <br />
+                    <p>{item.date}</p>
+                </div>
+            )}
         </div>
     )
 }
