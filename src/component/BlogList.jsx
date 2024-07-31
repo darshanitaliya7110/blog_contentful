eb44c6d1c7b95574ba1a4c033eaebb1c899dcbc6
darshanitaliya7110 <darshan.italiya@pixer.digital>
2024-07-31 15:42:47 +0530

    add sidebar for direct link

--- src/component/BlogList.jsx
+++ src/component/BlogList.jsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { BLOCKS } from '@contentful/rich-text-types';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
@@ -20,31 +21,53 @@ const BlogList = ({ blogData }) => {
     };
 
 
+    const headingList = []
+    const options = {
+        renderNode: {
+            [BLOCKS.HEADING_2]: (node) => {
+
+                return node.content && node.content.map((item, i) => {
+                    const slugify = str => str.toLowerCase().trim().replace(/[^\w\s-]/g, '').replace(/[\s_-]+/g, '-').replace(/^-+|-+$/g, '');
+
+                    headingList.push({ title: item.value, slug: slugify(item.value) })
+                    return <div key={i}>
+                        <h1 id={slugify(item.value)} style={{ color: "red" }}><i><b>{item.value}</b></i></h1>
+                    </div>
+                })
+            }
+        }
+    };
+
+
 
     return (
-        <div>
-            {blogData?.map((item) =>
-                <div key={item._id}>
-                    <h1>{item.title}</h1>
-                    <p>{item.verified ? "verified" : "unverified"}</p>
-                    <p>{item.description}</p>
-                    <img src={item.image.url} alt={item.image.title} />
-                    <div>{documentToReactComponents(item.body.json)}</div>
-                    <span><p><img style={{ width: "30px", height: "30px" }} src={item.author.authorImage.url} alt={item.author.authorName} /> {item.author.authorName}</p></span>
-                    <br />
-                    <p>{formatDate(item.date)}</p>
-                    <div>Company list :
-                        {
-                            item.companyCollection.items.map((item) => <Link href={`/company/${item.sys.id}`} key={item.companyName}>
-                                {item.companyName}
-                            </Link>)
-                        }
+        <div style={{ display: "flex" }}>
+            <div >
+                {blogData?.map((item, i) =>
+                    <div key={item._id}>
+                        {item.title && <h1>{item.title}</h1>}
+                        {item.verified && <p>{item.verified ? "verified" : "unverified"}</p>}
+                        {item.description && <p>{item.description}</p>}
+                        {item.image.url && <img src={item.image.url} alt={item.image.title} />}
+                        {item.body.json && <div>{documentToReactComponents(item.body.json, options)}</div>}
+                        {item.author.authorImage.url && <span><p><img style={{ width: "30px", height: "30px" }} src={item.author.authorImage.url} alt={item.author.authorName} /> {item.author.authorName}</p></span>}
+                        <br />
+                        {item.date && <p>{formatDate(item.date)}</p>}
+                        {item.companyCollection.items && <div>Company list :
+                            {
+                                item.companyCollection.items.map((item) => <Link href={`/company/${item.sys.id}`} key={item.companyName}>
+                                    {item.companyName}
+                                </Link>)
+                            }
+                        </div>}
+                        <hr />
                     </div>
-                    <hr />
-                </div>
-            )
-            }
-        </div >
+                )
+                }
+            </div >
+            <div>{headingList.map((item) => <h4 key={item.slug}><Link href={`#${item.slug}`}>{item.title}</Link></h4>)
+            }</div>
+        </div>
     )
 }
 
